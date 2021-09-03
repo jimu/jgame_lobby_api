6fcfeda2f8100f9079345f28b275774c800682c2
Jim Urbas <git@jimu.net>
2021-09-03 06:44:59 -0500

    Passing match test

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -5,8 +5,6 @@ describe 'Matches API', type: :request do
   it 'returns all matches' do
     FactoryBot.create(:match, title: 'Match 001')
     FactoryBot.create(:match, title: 'Match 002')
-    FactoryBot.create(:match, title: 'Match 003')
-    FactoryBot.create(:match, title: 'Match 004')
     get '/matches'
     
     expect(response).to have_http_status(:success)
