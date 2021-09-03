dd48afd8b70cc39e42912cef1d028cda97258826
Jim Urbas <git@jimu.net>
2021-09-03 06:44:27 -0500

    add match test

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -1,1 +1,16 @@
-/home/jimu/matches_spec.rb
+require 'rails_helper'
+
+describe 'Matches API', type: :request do
+
+  it 'returns all matches' do
+    FactoryBot.create(:match, title: 'Match 001')
+    FactoryBot.create(:match, title: 'Match 002')
+    FactoryBot.create(:match, title: 'Match 003')
+    FactoryBot.create(:match, title: 'Match 004')
+    get '/matches'
+    
+    expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body).size).to eq(2)
+  end
+
+end
