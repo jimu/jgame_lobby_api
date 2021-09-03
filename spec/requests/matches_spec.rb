674d69369bbf80c39376bddbe25d9bc55a6a530c
Jim Urbas <git@jimu.net>
2021-09-03 14:22:50 -0500

    Tests - matches

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -2,13 +2,38 @@ require 'rails_helper'
 
 describe 'Matches API', type: :request do
 
-  it 'returns all matches' do
-    FactoryBot.create(:match, title: 'Match 001')
-    FactoryBot.create(:match, title: 'Match 002')
-    get '/matches'
-    
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+  describe 'GET /matches' do
+    it 'returns all matches' do
+
+      FactoryBot.create(:match, title: 'Match 001')
+      FactoryBot.create(:match, title: 'Match 002')
+      get '/matches'
+      
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
   end
 
+  describe 'POST /matches' do
+    it 'create a match' do
+      expect {
+        post '/matches', params: { match: {title: 'Match 005'}}
+      }.to change {Match.count}.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /matches/:id' do
+
+    let!(:match) { FactoryBot.create(:match, title: 'Match 001') }
+
+    it 'deletes a match' do
+        expect {
+          delete "/matches/#{match.id}"
+        }.to change {Match.count}.from(1).to(0)
+
+        expect(response).to have_http_status(:no_content)
+    end
+  end
 end
